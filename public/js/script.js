9576abe0fdcc5b11734877b25a92a09c36901cb0
Marcos-SCO <marcos_sco@outlook.com>
2020-12-02 14:57:29 -0300

    Modal item

--- public/js/script.js
+++ public/js/script.js
@@ -8,13 +8,15 @@ let modalQt = 1;
 
 let modalKey = 0;
 
+let body = document.querySelector('body');
 let content = document.getElementById('content');
+let modalItem = document.querySelector('.modal-item');
 let productContainer = document.querySelector('.product-container');
 // let slickContainer = document.querySelector('.slick-container');
 
 fetch(`${BASEApi}/lojas`)
     .then(data => data.json())
-    .then(data => formContent(data))
+    .then(data => formContent(data));
 
 function formContent(data) {
     data.map(({ id, name, products, thumb }) => {
@@ -40,10 +42,58 @@ function productContent(products) {
             <img src='${BASE}${photo}'>
                 <figcaption>
                     <h3>${description}</h3>
-                    <h2>${price}</h2>
+                    <h2>R$ ${price}</h2>
                 </figcaption>
-                <button>Comprar</button>
+                <button onclick="fetchForModal(${id});">Comprar</button>
             </figure>`
+    }).join('');
+}
+
+function fetchForModal(id) {
+    fetch(`${BASEApi}/produtos/${id}`)
+        .then(data => data.json())
+        .then(data => modalGenerate(data));
+}
+
+function modalGenerate(...data) {
+    console.log(data);
+    modalItem.innerHTML = '';
+    modalItem.classList.add('active');
+    body.classList.add('modal-active');
+    modalItem.innerHTML += `<span class='modal-close' onClick="modalItem.classList.remove('active');body.classList.remove('modal-active')">X</span>`;
+
+    let modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+
+    data.map(({ id, name, description, photo, price, grids, store }) => {
+        // Modal info 1
+        modalContent.innerHTML += `<div class='modal-info1'>
+        <img src='${photo}'>
+        <ul class='product-info'>
+        <li>- ${description}</li>
+        <li>- Marca: ${store}</li>
+        </ul>
+        </div>`;
+
+        // modal info 2
+        modalContent.innerHTML += `<div class='modal-info2'>
+        <span id='${id}' class='fav-item modal'></span>
+        <h5>Camisas</h5>
+        <h2>${name}</h2>
+        <h4>${description}</h4>
+        <h1>R$ ${price}</h1>
+        <div><h5>Tamanhos:</h5>`+
+            getGrids(grids)
+            + `</div>
+        </div>`;
+
+    });
+    return modalItem.append(modalContent);
+}
+
+function getGrids(grids) {
+    return grids.map(({ quantity, sizeId, sizeName }) => {
+        return `<span id='${sizeId}' class='grid-item' onclick="this.classList.toggle('active');">${sizeName}</span>`
     }).join('');
 }
 
