0819bd1e8959d0bcbaf67266476e083f5ad13f4e
Marcos-SCO <marcos_sco@outlook.com>
2020-12-02 11:47:04 -0300

    index

---
+++ public/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Houpa Teste FullStack Cart</title>
+    <link rel="stylesheet" type="text/css" href="slick/slick.css" />
+    <!-- // Slick default styling -->
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css" />
+    <!-- Custom slick -->
+    <link rel="stylesheet" type="text/css" href="css/slickCustom.css" />
+    
+    <!-- Style css -->
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+
+    <section id="content">
+        <div class="product-container">
+            <!-- <div class="slick-container responsive">
+                <div>
+                    <h3>1</h3>
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+                <div>
+                    <h3>4</h3>
+                </div>
+                <div>
+                    <h3>5</h3>
+                </div>
+                <div>
+                    <h3>6</h3>
+                </div>
+            </div> -->
+        </div>
+        <!-- <button  class="slick-prev slick-arrow" type="button" class="slick-prev"><</button>
+        <button class="slick-prev slick-arrow" type="button" class="slick-next">></button> -->
+    </section>
+
+    <!-- Slick -->
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+
+    <script src="js/xhr.js"></script>
+    <script src="js/script.js" defer></script>
+</body>
+
+</html>
